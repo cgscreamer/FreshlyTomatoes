a2479c791d0dd5445eef5d3350fada2dd0d2cebc
cgscreamer <31764109+cgscreamer@users.noreply.github.com>
2018-07-20 11:07:54 +0200

    Update media.py

--- media.py
+++ media.py
@@ -5,6 +5,7 @@ class Movie():
     VALID_RATINGS = [ "G", "PG", "PG-13", "R"]
 
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, director):
+    #Initialises the functions and aspects for each part of the movies html page   
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
